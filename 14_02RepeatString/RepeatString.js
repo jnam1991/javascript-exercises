1ab0753e63c9f3e0f884e2d605b370a46e3386f0
jnam1991 <nicholimiller7@gmail.com>
2024-04-15 14:52:12 -0700

    Look at solution to repeatString exercise
    Cant find solution on my own. Hopefully, I will forget
    tomorrow.

--- 14_02RepeatString/RepeatString.js
+++ 14_02RepeatString/RepeatString.js
@@ -1,5 +1,12 @@
-const RepeatString = function() {
-  
+const RepeatString = function(word, times) {
+    let word = 'hey';
+
+    for(i=0; i<times; i++){
+        
+    }
+    
+    
+    RepeatString('hey', 3)
 };
   
 // Do not edit below this line
