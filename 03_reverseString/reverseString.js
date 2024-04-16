b7de5faba064ac92cfef3324bfd65a4c8b28b2a1
jnam1991 <nicholimiller7@gmail.com>
2024-04-16 12:00:38 -0700

    Pass all tests of reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
+    let str = '';
+    
+    for(i=word.length-1; i>=0; i--){
+        str += word[i]
+    }
+    
+    return str;
 };
+
+reverseString('garbage');
 
 // Do not edit below this line
 module.exports = reverseString;
