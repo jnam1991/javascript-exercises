99c2de825cf4b109fbd33b9dd92045b7d82c7fe5
jnam1991 <nicholimiller7@gmail.com>
2024-04-16 11:48:41 -0700

    Pass all tests on repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+
+    let str = "";
+
+    for(i=0; i<times; i++){
+
+        str += word;
+    }
+
+    return str;
 
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
--- 14_02RepeatString/RepeatString.js
+++ 14_02RepeatString/RepeatString.js
@@ -1,13 +1,19 @@
 const RepeatString = function(word, times) {
-    let word = 'hey';
+    let str = '';
+
+    if(times<0){
+        return "ERROR";
+    }
 
     for(i=0; i<times; i++){
-        
+        str += word;
     }
     
+    return str;
     
-    RepeatString('hey', 3)
-};
+}
+
+RepeatString('hey', 3);
   
 // Do not edit below this line
 module.exports = RepeatString;
