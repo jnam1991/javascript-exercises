b45a24c31f62083308798e2a0f9e35d229c1c42f
jnam1991 <nicholimiller7@gmail.com>
2024-04-16 15:20:16 -0700

    Pass first test of removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, removedIndex) {
+
+    let newArray = array.splice(removedIndex-1, removedIndex-2);
+
+    return array;
 
 };
+
+removeFromArray([1, 2, 3, 4], 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
